1d50b6871fcb0763a4d5ed5cec4d1eacc3449f3f
JD Pagano <jonathanpagano@gmail.com>
2025-02-07 07:27:55 -0600

    wip

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+
+ln -s ~/src/coding_setup/.vimrc $HOME
+ln -s ~/src/coding_setup/.config $HOME
+ln -s ~/src/coding_setup/.tmux.conf $HOME
+ln -s ~/src/coding_setup/.zshrc $HOME
+ln -s ~/src/coding_setup/.sh $HOME
+ln -s ~/src/coding_setup/.ignore $HOME
+
+
+mkdir -p ~/.vim/backup_files
+mkdir ~/.vim/swap_files
+mkdir ~/.vim/undo_files
