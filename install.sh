27d6dac7952bd7dd10b9fe0acecefd9558c482f0
JD Pagano <jonathanpagano@gmail.com>
2025-02-07 07:58:09 -0600

    Pass nvim setting file

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ mv ~/.tmux.config ~/.tmux.config_bak
 cp ~/dotfiles/.zshrc ~/.zshrc
 cp ~/dotfiles/.tmux.conf ~/.tmux.conf
 cp ~/dotfiles/.sh/local.sh ~/.sh/local.sh
+cp ~/dotfiles/.config/nvim/init_cursor.lua ~/.config/nvim/init_cursor.lua
+
